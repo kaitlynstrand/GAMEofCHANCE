cc287db776afd1e6fd3507760c54a5b71deff112
Kaitlyn <kaitlyn.strand@gmail.com>
2017-11-27 14:18:55 -0600

    monday kaitlyn

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,34 +1,34 @@
 const db = require("../models");
 
-// Defining methods for the tasksController
+// Defining methods for the usersController
 module.exports = {
   findAll: function(req, res) {
-    db.Task
+    db.User
       .find(req.query)
       .sort({ id: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err))
   },
   findById: function(req, res) {
-    db.Task
+    db.User
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.Task
+    db.User
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.Task
+    db.User
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.Task
+    db.User
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))

ffef42d5aea1c4f974d87409d0247e4a0cf55816
Maximillian Teibel <GTCENTRAL@Maximillians-MacBook-Pro.local>
2017-11-27 18:47:16 -0600

    Navbar

---
+++ client/src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css"
+
+const Header = props =>
+<section className="hero">
+  <div className="hero-body">
+      <h1 className="title">
+        CHORE-US
+      </h1>
+  </div>
+</section>
+
+export default Header
---
+++ client/src/Components/Nav/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Nav = props => (
+	<div className="tabs is-fullwidth">
+  		<ul>
+    		<li><a href="/tasks">Tasks</a></li>
+    		<li><a href="/claim">Claim</a></li>
+    		<li><a href="/leaderboards">Leaderboards</a></li>
+  		</ul>
+	</div>
+	)
+
+export default Nav

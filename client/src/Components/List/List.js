be2cd65936c2f2c8b5b6bace4165afc9a3148631
Maximillian Teibel <GTCENTRAL@dhcp-10-102-240-94.wireless.northwestern.private>
2017-11-29 13:48:23 -0600

    Pulling from database

--- client/src/Components/List/List.js
+++ client/src/Components/List/List.js
@@ -3,10 +3,12 @@ import "./List.css";
 
 export const List = ({ children }) => {
   return (
-    <div className="list-overflow-container">
-      <ul className="list-group">
-        {children}
-      </ul>
+    <div className="card-table">
+    	<div className="content">
+      		<table className="table is-fullwidth is-striped">
+        		{children}
+      		</table>
+    	</div>
     </div>
   );
 };
--- client/src/pages/Tasks/tasks.js
+++ client/src/pages/Tasks/tasks.js
@@ -1,12 +1,43 @@
 import React, { Component } from "react"
 import Nav from "../../Components/Nav"
+import API from "../../utils/API";
+import { List, ListItem } from "../../Components/List"
 
 class Tasks extends Component {
+	state = {
+		tasks: [],
+		description: "",
+		dueDate: "",
+		points: "",
+	}
+
+	componentDidMount() {
+		this.loadTasks()
+	}
+
+	loadTasks = () => {
+		API.getTasks()
+		.then(res =>
+			this.setState({
+				tasks: res.data
+			})).catch(err => console.log(err))
+
+	}
 	render() {
 		return (
 			<div className="container">
             	<Nav />
-            	<div>Tasks</div>
+            	<List>
+            		{this.state.tasks.map(tasks => (
+            			<ListItem key={tasks._id}>
+            				<tr>
+            					<td>{tasks.description}</td>
+            					<td>{tasks.dueDate}</td>
+            					<td>{tasks.points}</td>
+            				</tr>
+            			</ListItem>
+            			))}
+      			</List>
             </div>
 		)
 	}

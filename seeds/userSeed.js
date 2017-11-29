5aa34162307e881b1ff4c6808d7bf23d87750e8e
Kaitlyn <kaitlyn.strand@gmail.com>
2017-11-29 12:31:44 -0600

    seed files

---
+++ seeds/userSeed.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const db = require('../models');
+mongoose.Promise = global.Promise;
+
+mongoose.connect(
+	process.env.MONGODB_URI || 'mongodb://localhost/chore-us',
+	{
+		useMongoClient: true
+	}
+);
+
+const userSeed = [
+	{
+		first_name: "Kaitlyn",
+		last_name: "Strand",
+		email: "kaitlyn.strand@gmail.com",
+		phone: "224-622-0800",
+		password: "NDcheer",
+		points_earned: "28"
+	},
+	{
+		first_name: "Maximillian",
+		last_name: "Teibel",
+		email: "mnteibel@gmail.com",
+		phone: "555-555-5555",
+		password: "raft4lyfe",
+		points_earned: "101"
+	},
+	{
+		first_name: "Esdras",
+		last_name: "Etrenne",
+		email: "esdras@gmail.com",
+		phone: "888-888-8888",
+		password: "tatata",
+		points_earned: "11"
+	}
+];
+
+db.User
+  .remove({})
+  .then(() => db.User.collection.insertMany(userSeed))
+  .then(data => {
+    console.log(data.insertedIds.length + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });

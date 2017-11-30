62771c47b42472ab0a3cec7cfb08718191b3a947
Kaitlyn <kaitlyn.strand@gmail.com>
2017-11-30 13:47:19 -0600

    auth wed

--- client/src/pages/SignIn/signin.js
+++ client/src/pages/SignIn/signin.js
@@ -3,57 +3,52 @@ import { Input, TextArea, FormBtn } from "../../Components/Form"
 
 class SignIn extends Component {
 	state = {
-		name: "",
+		username: "",
 		email: "",
 		password: "",
 
 	}
 
 	handleInputChange = event => {
-    	const { name, value } = event.target;
-    		this.setState({
-      			[name]: value
-    		});
-  	};
+		const { name, value } = event.target;
+		this.setState({
+			[name]: value
+		});
+	};
 
 	render() {
 		return (
-		<div className="container">
-			<label className="label">Name</label>
-				<Input 
-				value={this.state.name}
-				name="name"
-				onChange={this.handleInputChange}/>
-			<label className="label">Email</label>
-				<Input 
-				type="email"
-				value={this.state.email}
-				name="email"
-				onChange={this.handleInputChange}/>
-			<label className="label">Password</label>
-				<Input 
-				type="password"
-				value={this.state.password}
-				name="password"
-				onChange={this.handleInputChange}/>
+			<div className="container">
+				<label className="label">Email</label>
+					<Input 
+						type="email"
+						value={this.state.email}
+						name="email"
+						onChange={this.handleInputChange}/>
+				<label className="label">Password</label>
+					<Input 
+						type="password"
+						value={this.state.password}
+						name="password"
+						onChange={this.handleInputChange}/>
 			<div className="field">
 				<div className="control">
 					<label className="checkbox">
 						<input type="checkbox"></input>I agree to the <a href="#">terms and conditions</a>
 					</label>
-				</div>
 			</div>
+		</div>
 			<div className="field is-grouped">
 				<div className="control">
 					<FormBtn><a href="/groups">Submit</a></FormBtn>
 				</div>
 			</div>
-			<br>
-				<p>Create A New Account<a href="/signup">Sign Up</a></p>
-    		<p>Or go <a href="/">Home</a></p>
-		</div>
-		)
+				<br></br>
+			<p>Create A New Account <a href="/signup"> Sign Up</a></p>
+			<p>Or go <a href="/"> Home</a></p>
+			</div>
+			)
+		}
 	}
-}
 
-export default SignIn
+	export default SignIn
